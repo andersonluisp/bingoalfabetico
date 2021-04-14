175809c55452658d6c2ca1be825fa615be1bac70
andersonluisp <andersonluiscp@gmail.com>
2021-04-14 16:02:50 -0300

    Adicionado condição para quando todas as letras forem sorteadas. Melhorias no layout, tamanho do texto que aparece  cores do botão.

--- app/src/main/java/com/example/bingoalfabetico/MainActivity.kt
+++ app/src/main/java/com/example/bingoalfabetico/MainActivity.kt
@@ -21,19 +21,17 @@ class MainActivity : AppCompatActivity() {
         val btPlay = playButton
         val btBingo = bingoButton
 
-        embaralhaLetra()
+        embaralharLetras()
 
             btPlay.setOnClickListener {
             if (bingo == 1){
-                embaralhaLetra()
+                embaralharLetras()
                 bingo = 0
-                letraSorteada = alfabeto[contador].toString()
                 falarLetra()
                 playButton.setText("Sortear outra letra")
 
             }
             else {
-                letraSorteada = alfabeto[contador].toString()
                 falarLetra()
             }
         }
@@ -45,48 +43,58 @@ class MainActivity : AppCompatActivity() {
         }
 
     private fun falarLetra(){
-         when {
-            letraSorteada == "A" -> audioLetra = MediaPlayer.create(this, R.raw.a)
-            letraSorteada == "B" -> audioLetra = MediaPlayer.create(this, R.raw.b)
-            letraSorteada == "C" -> audioLetra = MediaPlayer.create(this, R.raw.c)
-            letraSorteada == "D" -> audioLetra = MediaPlayer.create(this, R.raw.d)
-            letraSorteada == "E" -> audioLetra = MediaPlayer.create(this, R.raw.e)
-            letraSorteada == "F" -> audioLetra = MediaPlayer.create(this, R.raw.f)
-            letraSorteada == "G" -> audioLetra = MediaPlayer.create(this, R.raw.g)
-            letraSorteada == "H" -> audioLetra = MediaPlayer.create(this, R.raw.h)
-            letraSorteada == "I" -> audioLetra = MediaPlayer.create(this, R.raw.i)
-            letraSorteada == "J" -> audioLetra = MediaPlayer.create(this, R.raw.j)
-            letraSorteada == "K" -> audioLetra = MediaPlayer.create(this, R.raw.k)
-            letraSorteada == "L" -> audioLetra = MediaPlayer.create(this, R.raw.l)
-            letraSorteada == "M" -> audioLetra = MediaPlayer.create(this, R.raw.m)
-            letraSorteada == "N" -> audioLetra = MediaPlayer.create(this, R.raw.n)
-            letraSorteada == "O" -> audioLetra = MediaPlayer.create(this, R.raw.o)
-            letraSorteada == "P" -> audioLetra = MediaPlayer.create(this, R.raw.p)
-            letraSorteada == "Q" -> audioLetra = MediaPlayer.create(this, R.raw.q)
-            letraSorteada == "R" -> audioLetra = MediaPlayer.create(this, R.raw.r)
-            letraSorteada == "S" -> audioLetra = MediaPlayer.create(this, R.raw.s)
-            letraSorteada == "T" -> audioLetra = MediaPlayer.create(this, R.raw.t)
-            letraSorteada == "U" -> audioLetra = MediaPlayer.create(this, R.raw.u)
-            letraSorteada == "V" -> audioLetra = MediaPlayer.create(this, R.raw.v)
-            letraSorteada == "W" -> audioLetra = MediaPlayer.create(this, R.raw.w)
-            letraSorteada == "X" -> audioLetra = MediaPlayer.create(this, R.raw.x)
-            letraSorteada == "Y" -> audioLetra = MediaPlayer.create(this, R.raw.y)
-            letraSorteada == "Z" -> audioLetra = MediaPlayer.create(this, R.raw.z)
-        }
-        audioLetra?.start()
-        textView.setText("LETRA ${letraSorteada}")
-        letrasSorteadas.add(letraSorteada)
-        contador++
+         if (contador > 25) {
+             bingo()
+             textView.setText("TODAS AS LETRAS FORAM SORTEADAS\n" +
+                     "A sequência das letras sorteadas foram:\n $letrasSorteadas\"")
+         }
+        else {
+             letraSorteada = alfabeto[contador].toString()
+             when {
+                 letraSorteada == "A" -> audioLetra = MediaPlayer.create(this, R.raw.a)
+                 letraSorteada == "B" -> audioLetra = MediaPlayer.create(this, R.raw.b)
+                 letraSorteada == "C" -> audioLetra = MediaPlayer.create(this, R.raw.c)
+                 letraSorteada == "D" -> audioLetra = MediaPlayer.create(this, R.raw.d)
+                 letraSorteada == "E" -> audioLetra = MediaPlayer.create(this, R.raw.e)
+                 letraSorteada == "F" -> audioLetra = MediaPlayer.create(this, R.raw.f)
+                 letraSorteada == "G" -> audioLetra = MediaPlayer.create(this, R.raw.g)
+                 letraSorteada == "H" -> audioLetra = MediaPlayer.create(this, R.raw.h)
+                 letraSorteada == "I" -> audioLetra = MediaPlayer.create(this, R.raw.i)
+                 letraSorteada == "J" -> audioLetra = MediaPlayer.create(this, R.raw.j)
+                 letraSorteada == "K" -> audioLetra = MediaPlayer.create(this, R.raw.k)
+                 letraSorteada == "L" -> audioLetra = MediaPlayer.create(this, R.raw.l)
+                 letraSorteada == "M" -> audioLetra = MediaPlayer.create(this, R.raw.m)
+                 letraSorteada == "N" -> audioLetra = MediaPlayer.create(this, R.raw.n)
+                 letraSorteada == "O" -> audioLetra = MediaPlayer.create(this, R.raw.o)
+                 letraSorteada == "P" -> audioLetra = MediaPlayer.create(this, R.raw.p)
+                 letraSorteada == "Q" -> audioLetra = MediaPlayer.create(this, R.raw.q)
+                 letraSorteada == "R" -> audioLetra = MediaPlayer.create(this, R.raw.r)
+                 letraSorteada == "S" -> audioLetra = MediaPlayer.create(this, R.raw.s)
+                 letraSorteada == "T" -> audioLetra = MediaPlayer.create(this, R.raw.t)
+                 letraSorteada == "U" -> audioLetra = MediaPlayer.create(this, R.raw.u)
+                 letraSorteada == "V" -> audioLetra = MediaPlayer.create(this, R.raw.v)
+                 letraSorteada == "W" -> audioLetra = MediaPlayer.create(this, R.raw.w)
+                 letraSorteada == "X" -> audioLetra = MediaPlayer.create(this, R.raw.x)
+                 letraSorteada == "Y" -> audioLetra = MediaPlayer.create(this, R.raw.y)
+                 letraSorteada == "Z" -> audioLetra = MediaPlayer.create(this, R.raw.z)
+             }
+             audioLetra?.start()
+             textView.setText("LETRA ${letraSorteada}")
+             letrasSorteadas.add(letraSorteada)
+             contador++
+         }
 
 
     }
 
-    private fun embaralhaLetra(){
+    private fun embaralharLetras(){
         alfabeto.shuffle()
     }
 
     private fun bingo() {
-        textView.setText("AS LETRAS SORTEADAS FORAM:\n $letrasSorteadas")
+        textView.setText("BINGO!!!\n" +
+                "AS LETRAS SORTEADAS FORAM:\n" +
+                " $letrasSorteadas.")
         bingo = 1
         contador = 0
         playButton.setText("Outra Rodada")
