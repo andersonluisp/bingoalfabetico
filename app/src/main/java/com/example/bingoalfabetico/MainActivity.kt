dc25cf712c7187ca18edf49c4014434950486f8b
andersonluisp <andersonluiscp@gmail.com>
2021-04-13 22:37:50 -0300

    criação da função de reproduzir arquivo mp3 criação da lista de letras e embaralhar lista definição de layout preliminar para teste de embaralhamento e de reprodução.

--- app/src/main/java/com/example/bingoalfabetico/MainActivity.kt
+++ app/src/main/java/com/example/bingoalfabetico/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.bingoalfabetico
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.media.MediaPlayer
+import android.view.MotionEvent
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private var letra: MediaPlayer? = null
+    val alfabeto: MutableList<String?> = mutableListOf("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btPlay = playButton
+
+        embaralhaLetra()
+
+        btPlay.setOnClickListener {
+            letra = MediaPlayer.create(this, R.raw.a);
+            falarLetra()
+            textView.setText("Letras embaralhadas para sorteio:\n${alfabeto.toString()}")
+        }
+
+        }
+
+    private fun falarLetra( ){
+        letra?.start()
+    }
+
+    private fun embaralhaLetra(){
+        alfabeto.shuffle()
     }
 }
